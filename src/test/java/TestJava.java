31a300b621abb8e77ccc799c907a93ffdd22fe2e
Sonal Agarwal <sonal.fd@gmail.com>
2021-01-16 22:29:30 +0530

    first commit

---
+++ src/test/java/TestJava.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class TestJava {
+ /*   public static void main(String args[])
+    {
+
+    }*/
+    @Test
+    public void abc()
+    {
+        System.out.println("first test");
+    }
+}
